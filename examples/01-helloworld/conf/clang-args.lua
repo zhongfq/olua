c716610eab39ad0655e1c3d17db2627c950f4067
O <codetypes@gmail.com>
2022-08-27 21:46:18 +0800

    collect headers from config file

--- examples/01-helloworld/conf/clang-args.lua
+++ examples/01-helloworld/conf/clang-args.lua
@@ -1,11 +1,9 @@
 clang {
-    headers = [[
-        #include "Example.h"
-    ]],
     flags = {
         '-DOLUA_DEBUG',
         '-Isrc',
         '-I../common',
+        '-I../lua',
         '-I../..',
     },
 }
